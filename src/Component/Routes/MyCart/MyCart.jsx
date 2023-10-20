b26507ead08fd1be999e183235e8cd9f20140ba9
mmahbubr0823 <mahbubfdtc@gmail.comm>
2023-10-20 12:49:42 +0600

    my cart page added

--- src/Component/Routes/MyCart/MyCart.jsx
+++ src/Component/Routes/MyCart/MyCart.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const MyCart = () => {
+    const loadedProductData = useLoaderData();
+
+
     return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, velit!
+        <div  className='grid grid-cols-3 gap-3'>
+            {
+                loadedProductData.map((productData, index) => <div key={productData._id}>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Name: {productData.name}!</h2>
+                            <p>Brand: {productData.brand}</p>
+                            <p>Price: {productData.price}</p>
+                            
+                        </div>
+                    </div>
+
+                </div>)
+            }
         </div>
     );
 };
